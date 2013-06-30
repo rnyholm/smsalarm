80c4d0d5de45b5554ccd815b5c3ea7300d72d8f4
Robert Nyholm <robert.nyholm@aland.net>
2013-06-30 20:14:13 +0300

    SA-8 tagit bort static finals och lagt till enumerationer istället för log prioriteter, även ändrat lite i loggningen

--- src/ax/ha/it/smsalarm/LogHandler.java
+++ src/ax/ha/it/smsalarm/LogHandler.java
@@ -26,6 +26,10 @@ import android.util.Log;
  * @date 2013-06-30
  */
 public class LogHandler {
+	// Enumeration for different LogPriorities needed to decide which kind of logging we're suppose to do
+	public enum LogPriorities {
+		ASSERT, DEBUG, ERROR, INFO, VERBOSE, WARN;
+	}
 	// Singleton instance of this class
 	private static LogHandler INSTANCE;
 
@@ -42,14 +46,6 @@ public class LogHandler {
 	// String to store ExternalStorageState in
 	private final String STATE = Environment.getExternalStorageState();
 
-	// Constants representing log priority
-	private final int ASSERT = 7;
-	private final int DEBUG = 3;
-	private final int ERROR = 6;
-	private final int INFO = 4;
-	private final int VERBOSE = 2;
-	private final int WARN = 5;
-
 	// Date and SimpleDateFormatter, to get current time in a certain format(LogCat like)
 	private Date today = Calendar.getInstance().getTime();
 	private SimpleDateFormat formatter = new SimpleDateFormat("yy-MM-dd HH:mm:ss.SSS");
@@ -63,7 +59,7 @@ public class LogHandler {
 	 */
 	private LogHandler() {
 		// Log information
-		this.logCatTxt(this.INFO, this.LOG_TAG + ":LogHandler()", "New instance of LogHandler created");
+		this.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":LogHandler()", "New instance of LogHandler created");
 	}
 
 	/**
@@ -81,66 +77,12 @@ public class LogHandler {
 	}
 
 	/**
-	 * Getter for the log priority constant ASSERT
-	 * 
-	 * @return The ASSERT constant, value 7
-	 */
-	public int getASSERT() {
-		return ASSERT;
-	}
-
-	/**
-	 * Getter for the log priority constant DEBUG
-	 * 
-	 * @return The DEBUG constant, value 3
-	 */
-	public int getDEBUG() {
-		return DEBUG;
-	}
-
-	/**
-	 * Getter for the log priority constant ERROR
-	 * 
-	 * @return The ERROR constant, value 6
-	 */
-	public int getERROR() {
-		return ERROR;
-	}
-
-	/**
-	 * Getter for the log priority constant INFO
-	 * 
-	 * @return The INFO constant, value 4
-	 */
-	public int getINFO() {
-		return INFO;
-	}
-
-	/**
-	 * Getter for the log priority constant VERBOSE
-	 * 
-	 * @return The VERBOSE constant, value 2
-	 */
-	public int getVERBOSE() {
-		return VERBOSE;
-	}
-
-	/**
-	 * Getter for the log priority constant WARN
-	 * 
-	 * @return The WARN constant, value 5
-	 */
-	public int getWARN() {
-		return WARN;
-	}
-
-	/**
 	 * Method to create a LogCat log message. If no valid priority value are
 	 * given it will log a message with <b><i>ERROR</i></b> priority with the
 	 * origin log message and current error.
 	 * 
 	 * @param priority
-	 *            Priority as integer see {@link #http
+	 *            Priority as LogPriorities see {@link #http
 	 *            ://www.http://developer.android
 	 *            .com/reference/android/util/Log.html}
 	 * @param logTag
@@ -148,35 +90,35 @@ public class LogHandler {
 	 * @param message
 	 *            Log message
 	 * 
-	 * @see #logCat(int, String, String, Throwable)
-	 * @see #logTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String, Throwable)
+	 * @see #logCat(LogPriorities, String, String, Throwable)
+	 * @see #logTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String, Throwable)
 	 */
-	public void logCat(int priority, String logTag, String message) {
+	public void logCat(LogPriorities priority, String logTag, String message) {
 		// Switch through the different log priorities and log the correct priority and message
 		switch (priority) {
-		case (ASSERT):// <-- 7
+		case ASSERT:
 			// HAS NO Log METHOD
 			break;
-		case (DEBUG):// <-- 3
+		case DEBUG:
 			Log.d(logTag, message);
 			break;
-		case (ERROR):// <-- 6
+		case ERROR:
 			Log.e(logTag, message);
 			break;
-		case (INFO):// <-- 4
+		case INFO:
 			Log.i(logTag, message);
 			break;
-		case (VERBOSE):// <-- 2
+		case VERBOSE:
 			Log.v(logTag, message);
 			break;
-		case (WARN):// <-- 5
+		case WARN:
 			Log.w(logTag, message);
 			break;
 		default:
 			// Invalid log priority detected log another log message with this error and the origin log message
-			this.logCat(this.ERROR, this.LOG_TAG + ":logCat()", "Log priority wasn't found, check that correct priority constant is used." + " Original logmessage came from: " + logTag + ", with message: " + message);
+			this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logCat()", "Log priority wasn't found, check that correct priority enumeration is used." + " Original logmessage came from: \"" + logTag + "\", with message: \"" + message + "\"");
 		}
 	}
 
@@ -186,7 +128,7 @@ public class LogHandler {
 	 * priority with the origin log message and current error.
 	 * 
 	 * @param priority
-	 *            Priority as integer see {@link #http
+	 *            Priority as LogPriorities see {@link #http
 	 *            ://www.http://developer.android
 	 *            .com/reference/android/util/Log.html}
 	 * @param logTag
@@ -196,35 +138,35 @@ public class LogHandler {
 	 * @param thr
 	 *            Throwable object
 	 * 
-	 * @see #logCat(int, String, String)
-	 * @see #logTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String, Throwable)
+	 * @see #logCat(LogPriorities, String, String)
+	 * @see #logTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String, Throwable)
 	 */
-	public void logCat(int priority, String logTag, String message, Throwable thr) {
+	public void logCat(LogPriorities priority, String logTag, String message, Throwable thr) {
 		// Switch through the different log priorities and log the correct priority, message and throwable
 		switch (priority) {
-		case (ASSERT):// <-- 7
+		case ASSERT:
 			// HAS NO Log METHOD
 			break;
-		case (DEBUG):// <-- 3
+		case DEBUG:
 			Log.d(logTag, message, thr);
 			break;
-		case (ERROR):// <-- 6
+		case ERROR:
 			Log.e(logTag, message, thr);
 			break;
-		case (INFO):// <-- 4
+		case INFO:
 			Log.i(logTag, message, thr);
 			break;
-		case (VERBOSE):// <-- 2
+		case VERBOSE:
 			Log.v(logTag, message, thr);
 			break;
-		case (WARN):// <-- 5
+		case WARN:
 			Log.w(logTag, message, thr);
 			break;
 		default:
 			// Invalid log priority detected log another log message with this error and the origin log message
-			this.logCat(this.ERROR, this.LOG_TAG + ":logCat()", "Log priority wasn't found, check that correct priority constant is used." + " Original logmessage came from: " + logTag + ", with message: " + message + " and throwable: " + thr);
+			this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logCat()", "Log priority wasn't found, check that correct priority enumeration is used." + " Original logmessage came from: \"" + logTag + "\", with message: \"" + message + "\" and throwable: \"" + thr + "\"");
 		}
 	}
 
@@ -235,7 +177,7 @@ public class LogHandler {
 	 * Throws different exceptions depending on error.
 	 * 
 	 * @param priority
-	 *            Priority as integer see {@link #http
+	 *            Priority as LogPriorities see {@link #http
 	 *            ://www.http://developer.android
 	 *            .com/reference/android/util/Log.html}
 	 * @param logTag
@@ -243,15 +185,15 @@ public class LogHandler {
 	 * @param message
 	 *            Log message
 	 * 
-	 * @see #logCat(int, String, String)
-	 * @see #logCat(int, String, String, Throwable)
-	 * @see #logCatTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String, Throwable)
+	 * @see #logCat(LogPriorities, String, String)
+	 * @see #logCat(LogPriorities, String, String, Throwable)
+	 * @see #logCatTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String, Throwable)
 	 * @see #checkDirAndFile(File, File, File)
 	 * @see #writeLogHeader(File)
 	 * @see #checkExternalStorageState()
 	 */
-	public void logTxt(int priority, String logTag, String message) {
+	public void logTxt(LogPriorities priority, String logTag, String message) {
 		// Get current time
 		this.today = Calendar.getInstance().getTime();
 
@@ -286,44 +228,43 @@ public class LogHandler {
 
 				// Switch through the different log priorities and log the correct priority and message
 				switch (priority) {
-				case (ASSERT):// <-- 7
+				case ASSERT:
 					// HAS NO Log METHOD
 					break;
-				case (DEBUG):// <-- 3
+				case DEBUG:// <-- 3
 					bW.write("DEBUG\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + this.EOL);
 					bW.newLine();
 					break;
-				case (ERROR):// <-- 6
+				case ERROR:
 					bW.write("ERROR\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + this.EOL);
 					bW.newLine();
 					break;
-				case (INFO):// <-- 4
+				case INFO:
 					bW.write("INFO\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + this.EOL);
 					bW.newLine();
 					break;
-				case (VERBOSE):// <-- 2
+				case VERBOSE:
 					bW.write("VERBOSE\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + this.EOL);
 					bW.newLine();
 					break;
-				case (WARN):// <-- 5
+				case WARN:
 					bW.write("WARN\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + this.EOL);
 					bW.newLine();
 					break;
 				default:
 					// Invalid log priority detected log another log message with this error and the origin log message
-					this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "Can't write to file because log priority wasn't found, check that correct priority constant is used");
+					this.logTxt(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "Can't write to file because log priority wasn't found, check that correct priority enumeration is used");
 				}
 
 				// Ensure that everything has been written to the file and close
 				bW.flush();
 				bW.close();
 			} catch (Exception e) {
-				e.printStackTrace();
-				this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "An Exception occurred during writing to file: " + this.TXT_LOG_FILE, e);
+				this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "An Exception occurred during writing to file: \"" + this.TXT_LOG_FILE + "\"", e);
 			}
 		} else {
 			// External storage is not available, log it
-			this.logCat(this.WARN, this.LOG_TAG + ":logTxt()", "Cannot log to file because external storage isn't available, check earlier logs fore more details");
+			this.logCat(LogPriorities.WARN, this.LOG_TAG + ":logTxt()", "Cannot log to file because external storage isn't available, check earlier logs fore more details");
 		}
 	}
 
@@ -334,7 +275,7 @@ public class LogHandler {
 	 * Throws different exceptions depending on error.
 	 * 
 	 * @param priority
-	 *            Priority as integer see {@link #http
+	 *            Priority as LogPriorities see {@link #http
 	 *            ://www.http://developer.android
 	 *            .com/reference/android/util/Log.html}
 	 * @param logTag
@@ -344,15 +285,15 @@ public class LogHandler {
 	 * @param thr
 	 *            Throwable object
 	 * 
-	 * @see #logCat(int, String, String)
-	 * @see #logCat(int, String, String, Throwable)
-	 * @see #logCatTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String, Throwable)
+	 * @see #logCat(LogPriorities, String, String)
+	 * @see #logCat(LogPriorities, String, String, Throwable)
+	 * @see #logCatTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String, Throwable)
 	 * @see #checkDirAndFile(File, File, File)
 	 * @see #writeLogHeader(File)
 	 * @see #checkExternalStorageState()
 	 */
-	public void logTxt(int priority, String logTag, String message, Throwable thr) {
+	public void logTxt(LogPriorities priority, String logTag, String message, Throwable thr) {
 		// Get current time
 		this.today = Calendar.getInstance().getTime();
 
@@ -387,44 +328,43 @@ public class LogHandler {
 
 				// Switch through the different log priorities and log the correct priority and message
 				switch (priority) {
-				case (ASSERT):// <-- 7
+				case ASSERT:
 					// HAS NO Log METHOD
 					break;
-				case (DEBUG):// <-- 3
+				case DEBUG:
 					bW.write("DEBUG\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + "#" + thr + this.EOL);
 					bW.newLine();
 					break;
-				case (ERROR):// <-- 6
+				case ERROR:
 					bW.write("ERROR\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + "#" + thr + this.EOL);
 					bW.newLine();
 					break;
-				case (INFO):// <-- 4
+				case INFO:
 					bW.write("INFO\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + "#" + thr + this.EOL);
 					bW.newLine();
 					break;
-				case (VERBOSE):// <-- 2
+				case VERBOSE:
 					bW.write("VERBOSE\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + "#" + thr + this.EOL);
 					bW.newLine();
 					break;
-				case (WARN):// <-- 5
+				case WARN:
 					bW.write("WARN\t\t" + this.formatter.format(this.today) + "\t" + logTag + "#" + message + "#" + thr + this.EOL);
 					bW.newLine();
 					break;
 				default:
 					// Invalid log priority detected log another log message with this error and the origin log message
-					this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "Can't write to file because log priority wasn't found, check that correct priority constant is used");
+					this.logTxt(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "Can't write to file because log priority wasn't found, check that correct priority enumeration is used");
 				}
 
 				// Ensure that everything has been written to the file and close
 				bW.flush();
 				bW.close();
 			} catch (Exception e) {
-				e.printStackTrace();
-				this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "An Exception occurred during writing to file: " + this.TXT_LOG_FILE, e);
+				this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "An Exception occurred during writing to file: \"" + this.TXT_LOG_FILE + "\"", e);
 			}
 		} else {
 			// External storage is not available, log it
-			this.logCat(this.WARN, this.LOG_TAG + ":logTxt()", "Cannot log to file because external storage isn't available, check earlier logs fore more details");
+			this.logCat(LogPriorities.WARN, this.LOG_TAG + ":logTxt()", "Cannot log to file because external storage isn't available, check earlier logs fore more details");
 		}
 	}
 
@@ -455,9 +395,8 @@ public class LogHandler {
 				// Try create new file
 				file.createNewFile();
 			} catch (IOException e) {
-				// Print stack trace and log it
-				e.printStackTrace();
-				this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "An IOException occurred during creation of file: " + this.TXT_LOG_FILE, e);
+				// Log exception
+				this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "An IOException occurred during creation of file: \"" + this.TXT_LOG_FILE + "\"", e);
 			}
 
 			// To write log header
@@ -473,15 +412,12 @@ public class LogHandler {
 				// Try create new file
 				file.createNewFile();
 			} catch (IOException e) {
-				// Print stack trace and log it
-				e.printStackTrace();
-				this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "An IOException occurred during creation of file: " + this.TXT_LOG_FILE, e);
+				// Log exception
+				this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "An IOException occurred during creation of file: \"" + this.TXT_LOG_FILE + "\"", e);
 			}
-
 			// To write log header
 			this.writeLogHeader(file);
 		}
-
 	}
 
 	/**
@@ -507,9 +443,8 @@ public class LogHandler {
 			bW.flush();
 			bW.close();
 		} catch (Exception e) {
-			// Print stack trace and log it
-			e.printStackTrace();
-			this.logCat(this.ERROR, this.LOG_TAG + ":logTxt()", "An Exception occurred during writing to file: " + this.TXT_LOG_FILE, e);
+			// Log exception
+			this.logCat(LogPriorities.ERROR, this.LOG_TAG + ":logTxt()", "An Exception occurred during writing to file: \"" + this.TXT_LOG_FILE + "\"", e);
 		}
 	}
 
@@ -518,7 +453,7 @@ public class LogHandler {
 	 * file.
 	 * 
 	 * @param priority
-	 *            Priority as integer see {@link #http
+	 *            Priority as LogPriorities see {@link #http
 	 *            ://www.http://developer.android
 	 *            .com/reference/android/util/Log.html}
 	 * @param logTag
@@ -526,12 +461,12 @@ public class LogHandler {
 	 * @param message
 	 *            Log message
 	 * 
-	 * @see #logCat(int, String, String)
-	 * @see #logCat(int, String, String, Throwable)
-	 * @see #logCatTxt(int, String, String, Throwable)
+	 * @see #logCat(LogPriorities, String, String)
+	 * @see #logCat(LogPriorities, String, String, Throwable)
+	 * @see #logCatTxt(LogPriorities, String, String, Throwable)
 	 * @see #checkExternalStorageState()
 	 */
-	public void logCatTxt(int priority, String logTag, String message) {
+	public void logCatTxt(LogPriorities priority, String logTag, String message) {
 		this.logCat(priority, logTag, message);
 		this.logTxt(priority, logTag, message);
 	}
@@ -541,7 +476,7 @@ public class LogHandler {
 	 * information to file.
 	 * 
 	 * @param priority
-	 *            Priority as integer see {@link #http
+	 *            Priority as LogPriorities see {@link #http
 	 *            ://www.http://developer.android
 	 *            .com/reference/android/util/Log.html}
 	 * @param logTag
@@ -551,12 +486,12 @@ public class LogHandler {
 	 * @param thr
 	 *            Throwable object
 	 * 
-	 * @see #logCat(int, String, String)
-	 * @see #logCat(int, String, String, Throwable)
-	 * @see #logCatTxt(int, String, String)
+	 * @see #logCat(LogPriorities, String, String)
+	 * @see #logCat(LogPriorities, String, String, Throwable)
+	 * @see #logCatTxt(LogPriorities, String, String)
 	 * @see #checkExternalStorageState()
 	 */
-	public void logCatTxt(int priority, String logTag, String message, Throwable thr) {
+	public void logCatTxt(LogPriorities priority, String logTag, String message, Throwable thr) {
 		this.logCat(priority, logTag, message, thr);
 		this.logTxt(priority, logTag, message, thr);
 	}
@@ -566,9 +501,9 @@ public class LogHandler {
 	 * whether its available for reading and writing, only reading, only writing
 	 * or not available at all.
 	 * 
-	 * @see #logCatTxt(int, String, String)
-	 * @see #logCatTxt(int, String, String, Throwable)
-	 * @see #logTxt(int, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String)
+	 * @see #logCatTxt(LogPriorities, String, String, Throwable)
+	 * @see #logTxt(LogPriorities, String, String)
 	 */
 	private void checkExternalStorageState() {
 		// If statements deciding if we can read/write, both or nothing from the external storage
@@ -579,14 +514,14 @@ public class LogHandler {
 			// We can only read the media
 			this.mExternalStorageAvailable = true;
 			this.mExternalStorageWriteable = false;
-			this.logCatTxt(this.WARN, this.LOG_TAG + ":checkExternalStorageState()", "Cannot write log, cause: External storage is not available for writing");
+			this.logCat(LogPriorities.WARN, this.LOG_TAG + ":checkExternalStorageState()", "Cannot write log, cause: External storage is not available for writing");
 		} else {
 			/* 
 			 * Something else is wrong. It may be one of many other states, but all we need
 			 * to know is we can neither read nor write 
 			 */
 			this.mExternalStorageAvailable = this.mExternalStorageWriteable = false;
-			this.logCatTxt(this.ERROR, this.LOG_TAG + ":checkExternalStorageState()", "Cannot write log, cause: External storage is not available for writing or reading");
+			this.logCatTxt(LogPriorities.ERROR, this.LOG_TAG + ":checkExternalStorageState()", "Cannot write log, cause: External storage is not available for writing or reading");
 		}
 	}
 }
