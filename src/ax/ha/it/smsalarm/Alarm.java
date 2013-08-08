487e23d393bb2b87bf17a79a0a4ba384c5705708
Robert Nyholm <robert.nyholm@aland.net>
2013-08-09 00:10:59 +0300

    SA-9, SA-18 implementerat en databashanterare och testat denna, funkar bra, har byggt alarm.java i stort sett färdig, javadoc

--- src/ax/ha/it/smsalarm/Alarm.java
+++ src/ax/ha/it/smsalarm/Alarm.java
@@ -33,10 +33,22 @@ public class Alarm {
 	String acknowledged;	// Localized datetime when the alarm was acknowledged
 	
 	/**
+	 * To create a new empty Alarm object.
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 */
+	public Alarm() {
+		// Log in debug purpose
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":Alarm()", "A new empty Alarm object was created");
+	}
+	
+	/**
 	 * To create a new Alarm object.
 	 * 
-	 * @param sender Alarms sender
-	 * @param message Alarm message
+	 * @param sender Alarm sender as <code>String</code>
+	 * @param message Alarm message as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public Alarm(String sender, String message) {
 		// Create and store a localized timestamp, this depends on users locale and/or settings
@@ -45,13 +57,36 @@ public class Alarm {
 		this.message = message;
 		this.acknowledged  = "-";
 		// Log in debug purpose
-		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":Alarm()", "A new Alarm object was created with following data [" + this.toString() + "]");
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":Alarm()", "A new Alarm object was created with following data: [" + this.toString() + "]");
 	}
+	
+	/**
+	 * To create a new Alarm object.
+	 * 
+	 * @param id Alarm id as <code>int</code>
+	 * @param rececived Alarm received date and time as <code>String</code>
+	 * @param sender Alarm sender as <code>String</code>
+	 * @param message Alarm message as <code>String</code>
+	 * @param acknowledged Alarm acknowledge date and time as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 */
+	public Alarm(int id, String received, String sender, String message, String acknowledged) {
+		this.id = id;
+		this.received = received;
+		this.sender = sender;
+		this.message = message;
+		this.acknowledged  = acknowledged;
+		// Log in debug purpose
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":Alarm()", "A new Alarm object was created with following data: [" + this.toString() + "]");
+	}	
 	
 	/**
 	 * To get Alarm's id.
 	 * 
 	 * @return Alarm id as <code>int</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public int getId() {
 		// Log in debug purpose
@@ -63,17 +98,21 @@ public class Alarm {
 	 * To set Alarm's id.
 	 * 
 	 * @param id Alarm id as <code>int</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public void setId(int id) {
 		this.id = id;
 		// Log in debug purpose
-		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setId()", "New alarm id has been set, id is:\"" + this.id + "\"");
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setId()", "Alarm id has been set, id is:\"" + this.id + "\"");
 	}
 
 	/**
 	 * To get date and time when an Alarm was received.
 	 * 
 	 * @return Datetime when an Alarm was received as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public String getReceived() {
 		// Log in debug purpose
@@ -85,10 +124,30 @@ public class Alarm {
 	 * To set date and time when an Alarm was received.
 	 * 
 	 * @param date Date when an Alarm was received as <code>Date</code>
+	 * 
+	 * @see #updateReceived()
+	 * @see #setReceived(String)
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public void setReceived(Date date) {
 		// Create and store a localized timestamp, this depends on users locale and/or settings
 		this.received = DateFormat.getDateTimeInstance().format(date);
+		// Log in debug purpose
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setReceived()", "Alarm received date and time has been set to:\"" + this.received + "\"");
+	}
+	
+	/**
+	 * To set date and time when an Alarm was received.
+	 * 
+	 * @param received Date and time when an Alarm was received as <code>String</code>
+	 * 
+	 * @see #updateReceived()
+	 * @see #setReceived(Date)
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 */
+	public void setReceived(String received) {
+		// Create and store a localized timestamp, this depends on users locale and/or settings
+		this.received = received;
 		// Log in debug purpose
 		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setReceived()", "Alarm received date and time has been set to:\"" + this.received + "\"");
 	}
@@ -97,6 +156,8 @@ public class Alarm {
 	 * To get Alarm's sender.
 	 * 
 	 * @return Sender as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public String getSender() {
 		// Log in debug purpose
@@ -108,17 +169,21 @@ public class Alarm {
 	 * To set Alarm's sender.
 	 * 
 	 * @param sender Sender as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public void setSender(String sender) {
 		this.sender = sender;
 		// Log in debug purpose
-		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setSender()", "New alarm sender has been set to:\"" + this.sender + "\"");
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setSender()", "Alarm sender has been set to:\"" + this.sender + "\"");
 	}
 
 	/**
 	 * To get Alarm's alarm message.
 	 * 
 	 * @return Alarm message as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public String getMessage() {
 		// Log in debug purpose
@@ -130,17 +195,21 @@ public class Alarm {
 	 * To set Alarm's alarm message.
 	 * 
 	 * @param message Alarm message as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public void setMessage(String message) {
 		this.message = message;
 		// Log in debug purpose
-		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":getId()", "New alarm message has been set to:\"" + this.message + "\"");
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":getId()", "Alarm message has been set to:\"" + this.message + "\"");
 	}
 
 	/**
 	 * To get Alarm's acknowledge time.
 	 * 
 	 * @return Acknowledge time as <code>String</code>
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public String getAcknowledged() {
 		// Log in debug purpose
@@ -152,6 +221,10 @@ public class Alarm {
 	 * To set date and time when an Alarm was acknowledged.
 	 * 
 	 * @param date Date when Alarm was acknowledged as <code>Date</code>
+	 * 
+	 * @see #updateAcknowledged()
+	 * @see #setAcknowledged(String)
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public void setAcknowledged(Date date) {
 		// Create and store a localized timestamp, this depends on users locale and/or settings
@@ -161,8 +234,28 @@ public class Alarm {
 	}
 	
 	/**
+	 * To set date and time when an Alarm was acknowledged.
+	 * 
+	 * @param acknowledged Date and time when Alarm was acknowledged as <code>String</code>
+	 * 
+	 * @see #updateAcknowledged()
+	 * @see #setAcknowledged(Date)
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 */
+	public void setAcknowledged(String acknowledged) {
+		// Create and store a localized timestamp, this depends on users locale and/or settings
+		this.acknowledged = acknowledged;
+		// Log in debug purpose
+		this.logger.logCat(LogPriorities.DEBUG, this.LOG_TAG + ":setAcknowledged()", "Alarm acknowledge date and time has been set to:\"" + this.acknowledged + "\"");
+	}
+	
+	/**
 	 * To update/set date and time when an Alarm was received. 
 	 * Date and time will be set to now.
+	 * 
+	 * @see #setReceived(Date)
+	 * @see #setReceived(String)
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */
 	public void updateReceived() {
 		// Create and store a localized timestamp, this depends on users locale and/or settings
@@ -174,6 +267,10 @@ public class Alarm {
 	/**
 	 * To update/set date and time when an Alarm was acknowledged. 
 	 * Date and time will be set to now.
+	 * 
+	 * @see #setAcknowledged(Date)
+	 * @see #setAcknowledged(String)
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
 	 */	
 	public void updateAcknowledged() {
 		// Create and store a localized timestamp, this depends on users locale and/or settings
---
+++ src/ax/ha/it/smsalarm/DatabaseHandler.java
@@ -0,0 +1,273 @@
+/**
+ * 
+ */
+package ax.ha.it.smsalarm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import ax.ha.it.smsalarm.LogHandler.LogPriorities;
+
+/**
+ * Class responsible for all <code>Database</code> access and handling.
+ * <code>Database</code> access and handling are done via the <code>SQLiteOpenHelper</code> class.
+ * 
+ * @author Robert Nyholm <robert.nyholm@aland.net>
+ * @version 2.1
+ * @since 2.1beta
+ * @date 2013-08-08
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+	// Log tag string
+	private static final String LOG_TAG = "DatabaseHandler";
+
+	// Object for logging
+	private static final LogHandler logger = LogHandler.getInstance();
+	
+	// Database Version
+	private static final int DB_VERSION = 1;
+	
+	// Database Name
+	private static final String DB_NAME = "alarmsManager";
+	
+	// Alarms Table name
+	private static final String TABLE_ALARMS = "alarms";
+	
+	// Alarms Table Column names
+	private static final String KEY_ID = "id";
+	private static final String KEY_RECEIVED = "received";
+	private static final String KEY_SENDER = "sender";
+	private static final String KEY_MESSAGE = "message";
+	private static final String KEY_ACKNOWLEDGED = "acknowledged";
+	
+	/**
+	 * Constructor for the <code>DatabaseHandler</code>, takes just context as
+	 * argument. Calling super classes constructor with <code>context, DATABASE_NAME, DATABASE_VERSION</code>.
+	 * 
+	 * @param context Context in which to create the <code>DatabaseHandler</code> object
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 */
+	public DatabaseHandler(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":DatabaseHandler()", "DatabaseHandler object has been created with following data:[DB_NAME:\"" + DB_NAME + "\", DB_VERSION:\"" +  DB_VERSION + "\" and context:\"" + context.toString() + "\"]");
+	}
+
+	/**
+	 * To create table table with columns representing an <code>Alarm</code>.
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 * @see ax.ha.it.smsalarm.Alarm
+	 */
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// Build up the query for creating the table
+        String CREATE_ALARMS_TABLE = "CREATE TABLE " + TABLE_ALARMS + "("+ KEY_ID + " INTEGER PRIMARY KEY," + KEY_RECEIVED + " TEXT,"
+            							+ KEY_SENDER + " TEXT," + KEY_MESSAGE + " TEXT," + KEY_ACKNOWLEDGED + " TEXT)";
+        // Run query
+        db.execSQL(CREATE_ALARMS_TABLE);
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "Table has been created from following query:\"" + CREATE_ALARMS_TABLE + "\"");        
+	}
+
+	/**
+	 * To upgrade the database.
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 */
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ALARMS);
+
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onUpgrade()", "Table has been dropped if it existed");     
+ 
+        // Create tables again
+        onCreate(db);
+	}
+
+	/**
+	 * To insert a new <code>Alarm</code> in the database.
+	 * 
+	 * @param alarm Alarm to be inserted in database
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 * @see ax.ha.it.smsalarm.Alarm
+	 */
+	public void addAlarm(Alarm alarm) {
+		// Get a writable database handle
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		// Fetch values from alarm and put the into a ContentValues variable
+	    ContentValues values = new ContentValues();    
+	    values.put(KEY_RECEIVED, alarm.getReceived());			// Date and time when alarm was received
+	    values.put(KEY_SENDER, alarm.getSender());				// Sender of the alarm
+	    values.put(KEY_MESSAGE, alarm.getMessage());			// Alarm message
+	    values.put(KEY_ACKNOWLEDGED, alarm.getAcknowledged());	// Date and time the alarm was acknowledged
+	 
+	    // Inserting Row
+	    db.insert(TABLE_ALARMS, null, values);
+	    db.close(); // Closing database connection	
+	    
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":addAlarm()", "An alarm with following data has been inserted into the database: [" + alarm.toString() + "]");  
+	}
+	
+	/**
+	 * To get a row from the database with the given <code>id</code> as an <code>Alarm</code> object.
+	 *  
+	 * @param id Find and return the <code>Alarm</code> object with this id given as <code>int</code>
+	 * 
+	 * @return <code>Alarm</code> object
+	 * 
+	 * @throws android.database.CursorIndexOutOfBoundsException If no row with the given id is found in the database
+	 * 
+	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 * @see ax.ha.it.smsalarm.Alarm
+	 */
+	public Alarm getAlarm(int id) throws android.database.CursorIndexOutOfBoundsException {
+		// Get a readable database handle
+	    SQLiteDatabase db = this.getReadableDatabase();
+	    
+	    // Create query and execute it, store result in cursor
+	    Cursor cursor = db.query(TABLE_ALARMS, new String[] { KEY_ID, KEY_RECEIVED, KEY_SENDER, KEY_MESSAGE, KEY_ACKNOWLEDGED }, 
+	    						 KEY_ID + "=?", new String[] { String.valueOf(id) }, null, null, null, null);
+	    
+	    // CHeck if we got any results from the query
+	    if (cursor != null) {
+			// Log in debug purpose
+			logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":getAlarm()", "Cursor is not NULL --> Got results from query");
+			// Move to first element so we can fetch data from the cursor later
+	    	cursor.moveToFirst();    	
+	    }
+	    // Create a new alarm object with data resolved from cursor
+	    Alarm alarm = new Alarm(Integer.parseInt(cursor.getString(0)), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4));
+	    // Return alarm
+	    return alarm;
+	}
+	
+	/**
+	 * To get all <code>Alarm</code> entries in database as a <code>List</code> of <code>Alarm</code> objects.
+	 * An empty list will be returned if no entries are found in database.
+	 * 
+	 * @return All entries in database as a <code>List</code> of <code>Alarm</code> objects
+	 * 
+ 	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 * @see ax.ha.it.smsalarm.Alarm
+	 */
+	public List<Alarm> getAllAlarm() {
+		// List to store all alarms in
+		List<Alarm> alarmList = new ArrayList<Alarm>();
+		
+		// Select All Query
+		String selectQuery = "SELECT  * FROM " + TABLE_ALARMS;
+
+		// Get a writable database handle
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		// Execute query, store result in cursor
+		Cursor cursor = db.rawQuery(selectQuery, null);
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":getAllAlarm()", "All alarms have been fetched from the database with the following query:\"" + selectQuery + "\"");
+ 
+		// Iterate through all rows and adding to list
+		if (cursor.moveToFirst()) {
+			do {
+				// Create a new alarm object and fill it with data from cursor
+				Alarm alarm = new Alarm();
+				alarm.setId(Integer.parseInt(cursor.getString(0)));
+				alarm.setReceived(cursor.getString(1));
+				alarm.setSender(cursor.getString(2));
+				alarm.setMessage(cursor.getString(3));
+				alarm.setAcknowledged(cursor.getString(4));
+				// Adding contact to list
+				alarmList.add(alarm);
+			} while (cursor.moveToNext());
+		}
+ 
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":getAllAlarm()", "Returning following list of Alarms:\"" + alarmList.toString() + "\"");		
+		// return contact list
+		return alarmList;
+	}
+	
+	/**
+	 * To count number of <code>Alarm</code>'s (entries) in database.
+	 * 
+	 * @return Number of <code>Alarm</code>'s (entries) in database as <code>int</code>
+	 * 
+ 	 * @see ax.ha.it.smsalarm.LogHandler#logCat(LogPriorities, String, String) logCat(LogPriorities, String, String)
+	 * @see ax.ha.it.smsalarm.Alarm
+	 */
+	public int getAlarmsCount() {
+		// Select all query
+        String countQuery = "SELECT  * FROM " + TABLE_ALARMS;
+        
+        // To store number of alarms in database in
+        int alarmsCount = 0;
+        
+        // Get a readable database handle
+        SQLiteDatabase db = this.getReadableDatabase();
+        // Execute query, store result in cursor
+        Cursor cursor = db.rawQuery(countQuery, null);
+        
+        // Store number of elements in cursor before closing it
+        alarmsCount = cursor.getCount();
+        cursor.close();
+ 
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":getAlarmsCount()", "Number of alarms(entries) in database are:\"" + Integer.toString(alarmsCount) + "\"");
+		
+        // Return number of entries in database
+        return alarmsCount;			
+	}
+	
+	/**
+	 * To update an existing <code>Alarm</code> entry in database. 
+	 * It finds correct entry to update on alarms <code>id</code>.
+	 * 
+	 * @param alarm <code>Alarm</code> to be inserted in database.
+	 * 
+	 * @return <code>int</code> DON*T REALLY KNOW!?!?
+	 */
+	public int updateAlarm(Alarm alarm) {
+		// Get a writable database handle
+	    SQLiteDatabase db = this.getWritableDatabase();
+	    
+		// Fetch values from alarm and put the into a ContentValues variable
+	    ContentValues values = new ContentValues();    
+	    values.put(KEY_RECEIVED, alarm.getReceived());			// Date and time when alarm was received
+	    values.put(KEY_SENDER, alarm.getSender());				// Sender of the alarm
+	    values.put(KEY_MESSAGE, alarm.getMessage());			// Alarm message
+	    values.put(KEY_ACKNOWLEDGED, alarm.getAcknowledged());	// Date and time the alarm was acknowledged
+	    
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":updateAlarm()", "Values have been retrieved from Alarm object and database entry is going to be updated");
+	 
+	    // Updating row
+	    return db.update(TABLE_ALARMS, values, KEY_ID + " = ?", new String[] { String.valueOf(alarm.getId()) });		
+	}
+	
+	/**
+	 * To delete an existing <code>Alarm</code> entry in database. 
+	 * It finds correct entry to delete on alarms <code>id</code>.
+	 * 
+	 * @param alarm <code>Alarm</code> to be deleted from database.
+	 */
+	public void deleteAlarm(Alarm alarm) {
+		// Get a writable database handle
+		SQLiteDatabase db = this.getWritableDatabase();
+    	db.delete(TABLE_ALARMS, KEY_ID + " = ?", new String[] { String.valueOf(alarm.getId()) });
+	    db.close();	
+	    
+		// Log in debug purpose
+		logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":deleteAlarm()", "Alarm:\"" + alarm.toString() + "\" has been deleted from database");
+	}
+}
