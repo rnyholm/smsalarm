30ede3fb2589ead3e64018057385a457514835a8
Robert Nyholm <robert.nyholm@aland.net>
2014-09-13 16:27:32 +0300

    #80 Improved the update handling, better support for several updates, better storing of current version number and made it easier to implement several update actions.

--- src/ax/ha/it/smsalarm/handler/UpdateHandler.java
+++ src/ax/ha/it/smsalarm/handler/UpdateHandler.java
@@ -15,12 +15,12 @@ import ax.ha.it.smsalarm.handler.PreferencesHandler.PrefKeys;
  * Responsible for any <b><i>update actions</i></b>. In other words if some changes has been made in current release of the application that needs to
  * have some code executed(action) done after update that code is managed by this class. Code that needs to be executed after update is placed in
  * {@link #onCreate()} method.<br>
- * To figure out if the application has been updated the <b><i>version code</i></b> is stored in <code>SharedPreferences</code>, and at the end of the
- * {@link #onCreate()} method the current version code is <b><i>always</i></b> stored to shared preferences, this way that code within
- * {@link #onCreate()} is executed more than one time.
+ * To figure out if the application has been updated the <b><i>version code</i></b> is stored in <code>SharedPreferences</code>, and depending on
+ * logic in the {@link #onCreate()} method correct updates are made, if any are needed.<br>
+ * If it's a <b><i>new installation</i></b> of Sms Alarm the current version code is stored into shared preferences.
  * 
  * @author Robert Nyholm <robert.nyholm@aland.net>
- * @version 2.2.1
+ * @version 2.3.1
  * @since 2.2.1
  */
 public class UpdateHandler extends Application {
@@ -31,12 +31,15 @@ public class UpdateHandler extends Application {
 	private final LogHandler logger = LogHandler.getInstance();
 	private final PreferencesHandler prefHandler = PreferencesHandler.getInstance();
 
+	// Base value for a fresh installation
+	private final int NEW_INSTALLATION = -1;
+
+	// Different update code levels, named as code level limit for update and a short description
+	private final int LVL_9_CHANGE_DATATYPE = 9;
+
 	// To store both the current and old version code in
 	private int currentVersionCode;
 	private int oldVersionCode;
-
-	// To indicate if update actions has been made
-	private boolean updateActionMade = false;
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -45,47 +48,55 @@ public class UpdateHandler extends Application {
 		super.onCreate();
 
 		try {
-			// Get both the current and the old version codes, if it's a new installation(no old
-			// version code exists)
-			// the default value to be fetched from the shared preferences is set to the current
-			// version code
+			// Get both the current and the old version codes, if it's a new installation(no old version code exists)
+			// the default value to be fetched from the shared preferences is set to the current version code
 			currentVersionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
-			oldVersionCode = (Integer) prefHandler.getPrefs(PrefKeys.SHARED_PREF, PrefKeys.VERSION_CODE, DataTypes.INTEGER, this, -1);
+			oldVersionCode = (Integer) prefHandler.getPrefs(PrefKeys.SHARED_PREF, PrefKeys.VERSION_CODE, DataTypes.INTEGER, this, NEW_INSTALLATION);
 
-			// Only if old version number is less than 9, in version 9 a
-			// list of primary listen numbers is used instead of a string
-			if (oldVersionCode < 9) {
-				// Some logging
-				logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "Old version code is lower than 9, need to move existing PRIMARY sms number to the new list of PRIMARY sms numbers. Old version code is: \"" + oldVersionCode + "\"");
+			// If it's not a new installation
+			if (oldVersionCode != NEW_INSTALLATION) {
+				// Only if old version number is less than 9, in version 9 a list of primary listen numbers is used instead of a string
+				if (oldVersionCode < LVL_9_CHANGE_DATATYPE) {
+					logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "Old version code is lower than 9, need to move existing PRIMARY sms number to the new list of PRIMARY sms numbers. Old version code is: \"" + oldVersionCode + "\"");
 
-				// Get the primary sms number from the shared preferences, also get the new list of
-				// primary sms numbers from the shared preferences
-				String primarySmsNumber = (String) prefHandler.getPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBER_KEY, DataTypes.STRING, this);
-				List<String> primarySmsNumbers = (List<String>) prefHandler.getPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBERS_KEY, DataTypes.LIST, this);
+					// Get the primary sms number from the shared preferences, also get the new list of primary sms numbers from the shared
+					// preferences
+					String primarySmsNumber = (String) prefHandler.getPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBER_KEY, DataTypes.STRING, this);
+					List<String> primarySmsNumbers = (List<String>) prefHandler.getPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBERS_KEY, DataTypes.LIST, this);
 
-				// If there is an existing number, add it to the list of primary sms numbers
-				// and set an empty string to the primary sms number as it's not used any more
-				if (primarySmsNumber.length() != 0) {
-					logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "The existing PRIMARY sms number: \"" + primarySmsNumber + "\" is about to be moved to the new list of PRIMARY sms numbers");
-					primarySmsNumbers.add(primarySmsNumber);
-					primarySmsNumber = "";
+					// If there is an existing number, add it to the list of primary sms numbers and set an empty string to
+					// the primary sms number as it's not used any more
+					if (primarySmsNumber.length() != 0) {
+						logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "The existing PRIMARY sms number: \"" + primarySmsNumber + "\" is about to be moved to the new list of PRIMARY sms numbers");
+						primarySmsNumbers.add(primarySmsNumber);
+						primarySmsNumber = "";
+					}
+
+					// Store both the list of primary sms numbers and the empty string of primary sms number not in use anymore
+					prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBER_KEY, primarySmsNumber, this);
+					prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBERS_KEY, primarySmsNumbers, this);
+
+					// Store the update code level to shared preferences, in this way if we have update on higher code level that will be run next
+					// time this method is executed
+					logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "Update actions has been made and the update level version code: \"" + LVL_9_CHANGE_DATATYPE + "\" is about to be stored to the shared preferences");
+					prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.VERSION_CODE, LVL_9_CHANGE_DATATYPE, this);
+
+					// Setting old version code to this update code level, this let's the next update action to take place
+					oldVersionCode = LVL_9_CHANGE_DATATYPE;
 				}
 
-				// Store both the list of primary sms numbers and the empty string of primary sms
-				// number not in use anymore
-				prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBER_KEY, primarySmsNumber, this);
-				prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.PRIMARY_LISTEN_NUMBERS_KEY, primarySmsNumbers, this);
-
-				// At last flag the boolean to true to indicate that update actions has been
-				// successfully made
-				updateActionMade = true;
-			}
-
-			// Only update version code in shared preferences if any update actions has been made
-			if (updateActionMade) {
-				logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "Update actions has been made and the current version code: \"" + currentVersionCode + "\" is about to be stored to the shared preferences");
+				// No update actions needed except for storing the latest version code
+				// The old version code is larger than the latest update level code, this tells us that all updates has been done
+				if (oldVersionCode >= LVL_9_CHANGE_DATATYPE && oldVersionCode < currentVersionCode) {
+					logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "All updates went through or no updates were needed, storing the current version code: \"" + currentVersionCode + "\" is about to be stored to the shared preferences");
+					prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.VERSION_CODE, currentVersionCode, this);
+				}
+			} else {
+				// It's a new installation, just store the recent versioncode.
+				logger.logCat(LogPriorities.DEBUG, LOG_TAG + ":onCreate()", "No updates needed as this installation is a new one, the current version code: \"" + currentVersionCode + "\" is about to be stored to the shared preferences");
 				prefHandler.setPrefs(PrefKeys.SHARED_PREF, PrefKeys.VERSION_CODE, currentVersionCode, this);
 			}
+
 		} catch (NameNotFoundException e) {
 			logger.logCatTxt(LogPriorities.ERROR, LOG_TAG + ":onCreate()", "Name of application package could not be found", e);
 		} catch (IllegalArgumentException e) {
