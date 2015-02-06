6a4c5b97548a983a8c5bc8b75c71818f24833811
Robert Nyholm <robert.nyholm@aland.net>
2015-02-07 00:02:20 +0200

    #119 Testcases has been added for the Util.class

---
+++ test/ax/ha/it/smsalarm/util/UtilTest.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2015 Robert Nyholm. All rights reserved.
+ */
+package ax.ha.it.smsalarm.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+/**
+ * Test class for {@link Util} and it's methods.
+ * 
+ * @author Robert Nyholm <robert.nyholm@aland.net>
+ * @version 2.3.1
+ * @since 2.3.1
+ */
+public class UtilTest extends TestCase {
+
+	private List<String> foxList;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		foxList = Arrays.asList("The", "quick", "Brown", "fOX", "jumpS", "oVEr", "the", "lazy", "dog");
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		foxList = null;
+	}
+
+	public void testExistsInIgnoreCases() {
+		assertTrue(Util.existsInIgnoreCases("JuMpS", foxList));
+		assertTrue(Util.existsInIgnoreCases("LAzy", foxList));
+		assertTrue(Util.existsInIgnoreCases("doG", foxList));
+		assertTrue(Util.existsInIgnoreCases("QuicK", foxList));
+		assertFalse(Util.existsInIgnoreCases("not", foxList));
+		assertFalse(Util.existsInIgnoreCases("Thee", foxList));
+		assertFalse(Util.existsInIgnoreCases("12k7f", foxList));
+		assertFalse(Util.existsInIgnoreCases("FiSH", foxList));
+	}
+
+	public void testExistsInConsiderCases() {
+		assertTrue(Util.existsInConsiderCases("The", foxList));
+		assertTrue(Util.existsInConsiderCases("fOX", foxList));
+		assertTrue(Util.existsInConsiderCases("oVEr", foxList));
+		assertTrue(Util.existsInConsiderCases("Brown", foxList));
+		assertFalse(Util.existsInConsiderCases("brown", foxList));
+		assertFalse(Util.existsInConsiderCases("jumps", foxList));
+		assertFalse(Util.existsInConsiderCases("fOx", foxList));
+		assertFalse(Util.existsInConsiderCases("LAzy", foxList));
+
+	}
+
+	public void testGetFileName() {
+		assertEquals("", Util.getFileName(null));
+		assertEquals("", Util.getFileName(""));
+		assertEquals("ledarlarm.wma", Util.getFileName("/storage/sdcard0/download/ledarlarm.wma"));
+		assertEquals("ledar larm.wma", Util.getFileName("/storage/sdcard0/download/ledar larm.wma"));
+		assertEquals("le*[darlarm.wma", Util.getFileName("/storage/sdcard0/download/le*[darlarm.wma"));
+		assertEquals("וצה1234_.wma", Util.getFileName("/storage/sdcard0/download/וצה1234_.wma"));
+		assertEquals("le-dar-la_rm.wma", Util.getFileName("/storage/sdcard0/download/le-dar-la_rm.wma"));
+		assertEquals("ledarlarm", Util.getFileName("/storage/sdcard0/download/ledarlarm"));
+		assertEquals("ledarlarm.wma", Util.getFileName("/storage/download/ledarlarm.wma"));
+		assertEquals("ledarlarm.wma", Util.getFileName("/storage/ledarlarm.wma"));
+		assertEquals("ledarlarm.wma", Util.getFileName("/ledarlarm.wma"));
+		assertEquals("ledarlarm.wma", Util.getFileName("ledarlarm.wma"));
+	}
+
+	public void testGetBaseFileName() {
+		assertEquals("", Util.getBaseFileName(null));
+		assertEquals("", Util.getBaseFileName(""));
+		assertEquals("ledarlarm", Util.getBaseFileName("/storage/sdcard0/download/ledarlarm.wma"));
+		assertEquals("ledar larm", Util.getBaseFileName("/storage/sdcard0/download/ledar larm.wma"));
+		assertEquals("le*[darlarm", Util.getBaseFileName("/storage/sdcard0/download/le*[darlarm.wma"));
+		assertEquals("וצה1234_", Util.getBaseFileName("/storage/sdcard0/download/וצה1234_.wma"));
+		assertEquals("le-dar-la_rm", Util.getBaseFileName("/storage/sdcard0/download/le-dar-la_rm.wma"));
+		assertEquals("ledarlarm", Util.getBaseFileName("/storage/sdcard0/download/ledarlarm"));
+		assertEquals("ledarlarm", Util.getBaseFileName("/storage/download/ledarlarm.wma"));
+		assertEquals("ledarlarm", Util.getBaseFileName("/storage/ledarlarm.wma"));
+		assertEquals("ledarlarm", Util.getBaseFileName("/ledarlarm.wma"));
+		assertEquals("ledarlarm", Util.getBaseFileName("ledarlarm.wma"));
+	}
+}
